5c98bb996a0fde2a6e9b64873dc4ef7be41dadaa
Juan Manuel <jncjaen@correo.ugr.es>
2016-01-16 21:36:07 +0100

    Buscando el error al calcular scanner relativo a la posicion

--- Practica3DBA/src/ia/IAAgentes.java
+++ Practica3DBA/src/ia/IAAgentes.java
@@ -37,11 +37,32 @@ public class IAAgentes {
         System.out.println("--> Estoy en: "+ gps.getX() + "," + gps.getY());
         System.out.println("Voy a: "+ objetivo.getX() + "," + objetivo.getY());
         System.out.println("Los valores del scanner son:");
-        for(int i=0; i<tamanho; i++){
-            for(int j=0; j<tamanho; j++){
-                scanner[i][j] = GetScanner(i, j);
-                System.out.print(scanner[i][j]+" ");
-                if(minimo>scanner[i][j]||(i<=medio+1&&i>=medio-1&&j<=medio+1&&j>=medio-1)){
+        for (int i=0; i<tamanho; i++){
+            for (int j=0; j<tamanho; j++){
+                System.out.print(GetScanner(i, j)+";"+sensores[i][j] + "\t");
+            }
+            System.out.println();
+            
+        }
+        System.out.println("Valor medio: "+medio +" Ventana");
+        int [][] acotado = new int[3][3];
+        int ai=0, aj=0;
+        for(int i=medio-1; i<medio+2; i++){
+            for (int j=medio-1; j<medio+2; j++){
+                acotado[ai][aj] = sensores[i][j];
+                System.out.println("["+i+";"+j+"]\t");
+                aj++;
+            }
+            System.out.println();
+            aj=0;
+            ai++;
+        }
+        
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                //scanner[i][j] = GetScanner(i, j);
+                //System.out.print(scanner[i][j]+";"+sensores[i][j]+"\t");
+                if(minimo>scanner[i][j]){
                     di=i;
                     dj=j;
                     minimo=scanner[i][j];
@@ -49,10 +70,12 @@ public class IAAgentes {
             }
             System.out.println();
         }
-        
-        di = di%3;
-        dj = dj%3;
+        di = di;
+        dj = dj;
         String[][] directions = {{"NW","N","NE"},{"W","GOAL","E"},{"SW","S","SE"}}; 
+        System.out.println("di="+di+"; dj="+dj+"; --->"+ directions[di][dj]);
+        
+        
         
         return directions[di][dj];
     }
