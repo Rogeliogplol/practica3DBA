7f074e9c7a1ad98921479845b6b2914192d89728
FcoSueza <fcosueza@mailoo.org>
2016-01-16 13:15:49 +0100

    Clase IAControlador

---
+++ Practica3DBA/src/ia/IAControlador.java
@@ -0,0 +1,29 @@
+package ia;
+
+/**
+ * Clase que implementa la IA del agente controlador.
+ * 
+ * 
+ * @author Fco Sueza Rodríguez
+ */
+
+public class IAControlador {
+    
+    
+    public IAControlador(){};
+    
+    public  Posicion calculateGoalPos(Posicion original) {
+        
+        Posicion posGoal = new Posicion(original.getX(), original.getY());
+        
+        if (original.getY() == 99)
+            original.setY(0);
+        else
+            original.setY(99);
+        
+        return posGoal;
+    }
+    
+    
+    
+}
