f3c73cba015167875f1eb29f648171d35c489ec0
FcoSueza <fcosueza@mailoo.org>
2016-01-17 13:06:53 +0100

    Creación Clase Algorithm (abstract para manejar los dos algoritmo) y copia de algoritmos (falta adpatar)

---
+++ Practica3DBA/src/algorithm/Algorithm.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import ia.Posicion;
+
+/**
+ *  Clase abstracta que maneja los dos algoritmos
+ * 
+ * @author Fco Sueza Rodríguez
+ */
+
+public abstract class Algorithm {
+    
+    protected final String[][] directions = {{"NW","N","NE"},{"W","GOAL","E"},{"SW","S","SE"}};
+    Posicion objetivo;
+    
+    
+    public abstract process(int [][] scanner, )
+    
+    
+    
+    
+    
+    
+}
